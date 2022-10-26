230a1903f460bc707e7100fda6da14a9a65999bd
Junesik (Tony) Han <jhan80@my.centennialcollege.ca>
2022-10-26 15:59:55 -0400

    Delete button fully functional

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -60,19 +60,53 @@ export function displayEditPage(req, res, next) {
     /*****************
      * ADD CODE HERE *
      *****************/
+    // retrieve book id
+    let id = req.params.id;
 
-}
+    booksModel.findById(id, (err, book) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+        res.render('index', {title: 'Edit Book', page: 'books/edit', book: book});
+    });
+};
 
 // POST - process the information passed from the details form and update the document
 export function processEditPage(req, res, next) {
     /*****************
     * ADD CODE HERE *
     *****************/
-}
+    // retrieve book id
+    // let id = req.params.id;
+    // let newBook = booksModel({
+    //     _id: req.body.id,
+    //     author: req.body.author,
+    //     published: req.body.published,
+    //     description: req.body.description,
+    //     price: req.body.price
+    // });
+
+    // booksModel.updateOne({_id: id}, newBook, (err, Book) => {
+    //     if(err){
+    //         console.error(err);
+    //         res.end(err);
+    //     };
+    //     res.redirect('/books/list');
+    // });
+};
 
 // GET - process the delete by user id
 export function processDelete(req, res, next) {
     /*****************
-  * ADD CODE HERE *
-  *****************/
+    * ADD CODE HERE *
+    *****************/
+   let id = req.params.id;
+   booksModel.remove({_id: id}, (err) => {
+    if(err){
+        console.error(err);
+        res.end(err);
+    };
+    res.redirect('/books/list');
+   })
 }
